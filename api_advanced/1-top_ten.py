0b108166b8b3fa45f03617d7f6067f5829727aab
ibyishaka <t.ibyishaka@alustudent.com>
2025-02-24 08:34:37 +0200

    top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,7 +4,10 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a given subreddit and return 'OK'."""
+    """
+    Print the titles of the first 10 hot posts for a given subreddit
+    and return " OK "
+    """
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -23,4 +26,3 @@ if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
         top_ten(sys.argv[1])
-
